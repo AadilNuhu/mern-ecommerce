1c3b3050beb2637ec31e5896e1d2a076c3d5baab
Aadil Nuhu <aadilnuhu122@gmail.com>
2025-09-23 15:41:10 -0700

    Add typedarray and util-deprecate modules with tests and documentation
    - Introduced 	ypedarray module as a polyfill for TypedArrays in older browsers, including its package.json, readme, and test files.
    - Added util-deprecate module to provide deprecation warnings in both Node.js and browser environments, along with its documentation and tests.
    - Implemented tests for both modules to ensure functionality and correctness.
    - Updated package-lock.json to reflect new dependencies and versions.

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const ProductSchema = mongoose.Schema({
     productName: {type:String, required:true},
     description: {type:String, required:true},
-    price: {type:String, required:true}
+    price: {type:String, required:true},
+    image : String
 })
 
 module.exports = mongoose.model('product',ProductSchema)
