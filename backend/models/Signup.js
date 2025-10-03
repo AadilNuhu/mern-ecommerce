d01d5b56aaad486fe7b548c1a9f1158b4ce57a08
Aadil Nuhu <aadilnuhu122@gmail.com>
2025-10-03 10:39:06 -0700

    `Update config.js to exit with non-zero status code on error and update Signup.js to fix formatting and add missing semicolons.`

--- backend/models/Signup.js
+++ backend/models/Signup.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const SignupSchema = mongoose.Schema({
+const SignupSchema = mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique:true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
-    }
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps: true})
-
-module.exports = mongoose.model('User', SignupSchema)
+module.exports = mongoose.model("User", SignupSchema);
